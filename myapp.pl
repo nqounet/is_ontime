6a5da0487386f17e70e85e5864d3baf25991bbe5
nqounet <nqou.net@gmail.com>
2013-05-11 16:59:38 +0900

    括弧を省略しないようにした

--- myapp.pl
+++ myapp.pl
@@ -16,15 +16,13 @@ my ($target, $begin, $end) = @ARGV;
 my $result = $app->is_ontime($target, $begin, $end);
 if (defined $result) {
   if ($result) {
-    say encode_utf8
-      qq{$target 時は $begin 時から $end 時の間に含まれる};
+    say encode_utf8(qq{$target 時は $begin 時から $end 時の間に含まれる});
   }
   else {
-    say encode_utf8
-      qq{$target 時は $begin 時から $end 時の間に含まれない};
+    say encode_utf8(qq{$target 時は $begin 時から $end 時の間に含まれない});
   }
 }
 else {
-  say encode_utf8 '入力が正しくありません';
-  say encode_utf8 '使い方：' . $0 . ' targettime begintime endtime';
+  say encode_utf8('入力が正しくありません');
+  say encode_utf8('使い方：' . $0 . ' targettime begintime endtime');
 }
